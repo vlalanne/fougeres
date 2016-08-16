089f310c4e258a5908385ded6dee82ca7768e44e
Vincent LALANNE <vlalanne90@gmail.com>
2016-08-16 14:09:16 +0200

    NEW surroundings page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { MainComponent } from './main';
+import { SurroundingsComponent } from './surroundings';
 
 const routes: RouterConfig = [
+   { path: '', component: MainComponent},
    { path: 'main', component: MainComponent},
-   { path: '', component: MainComponent}
+   { path: 'surroundings', component: SurroundingsComponent}
 ];
 
 export const APP_ROUTER_PROVIDERS  = [
---
+++ src/app/surroundings/surroundings.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-surroundings',
+  templateUrl: 'surroundings.component.html',
+  styleUrls: ['surroundings.component.css']
+})
+export class SurroundingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
